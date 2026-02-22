1910ea06b1fa14f34b861988b1fc4f1674acd366
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2026-02-22 22:40:24 +0900

    first commit

--- obj/Debug/net10.0/Hello.AssemblyInfo.cs
+++ obj/Debug/net10.0/Hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78a17bb0758e531c46c314b47bd6a0f57f0f9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
