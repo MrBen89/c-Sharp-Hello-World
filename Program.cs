a78a17bb0758e531c46c314b47bd6a0f57f0f9b6
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2026-02-22 22:38:09 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//declaration
+string myVariable;
+myVariable = "My Value";
+
+Console.WriteLine(myVariable);
